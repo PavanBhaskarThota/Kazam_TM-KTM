25b516034969e2b13a49a482aa84e3416eee6aac
PavanBhaskarThota <pavanbhaskarthota05@gmail.com>
2025-02-08 19:05:30 +0530

    server fix

--- client/src/Api/index.ts
+++ client/src/Api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_URL || "http://localhost:7700",
+  baseURL: import.meta.env.REACT_APP_API_URL || "https://codesk-vy7w.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
